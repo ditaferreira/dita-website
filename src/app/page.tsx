7f6ad38d864a1634ed7eaa460205944f258b7459
Tupã Levi <tupa@earthguardians.org>
2025-10-22 17:11:58 -0300

    dita-finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -254,7 +254,7 @@ export default function HomePage() {
           {/* Dome Gallery Component - Full Width */}
           <div
             className="relative w-full flex items-center justify-center z-10"
-            style={{ height: '100vh', maxWidth: '100vw' }}
+            style={{ height: '70vh', maxWidth: '70vw' }}
           >
             <DomeGallery />
           </div>
